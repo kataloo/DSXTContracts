4250e6fb35744b98bf45010821476ae1219b9dd7
Gleb Zykov Air <glebzykov@gmail.com>
2019-04-06 17:11:38 +0300

    add withdraw for order function

--- contracts/CurrencyContract.sol
+++ contracts/CurrencyContract.sol
@@ -3,17 +3,27 @@ pragma solidity >=0.4.21 <0.6.0;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
-
 contract CurrencyContract {
     
     using SafeMath for uint256;
 
-    IERC20 currency;
+    IERC20 public currency;
 
-    mapping(address => uint256) balances;
+    address public executor;
 
-    constructor(address ERC20ContractAddress) public {
+    mapping(address => uint256) public balances;
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyExecutor() {
+        require(msg.sender == executor);
+        _;
+    }
+
+    constructor(address ERC20ContractAddress, address _executorContractAddress) public {
         currency = IERC20(ERC20ContractAddress);
+        executor = _executorContractAddress;
     }
 
     function deposit(uint _amount) public {
@@ -24,6 +34,11 @@ contract CurrencyContract {
     function withdraw(uint _amount) public {
         require(balances[msg.sender] >= _amount, "not enough tokens");
         currency.transfer(msg.sender, _amount);
+    }
+
+    function withdrawForOrder(uint256 _amount, address _from, address _to) public onlyExecutor {
+        balances[_from] = balances[_from].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
     }
 
 }
