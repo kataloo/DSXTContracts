969fdd0d9dfc3c1f2114e89140381538ef231cf5
Gleb Zykov Air <glebzykov@gmail.com>
2019-04-06 15:27:17 +0300

    add fake test

---
+++ test/CurrencyContractTest.js
@@ -0,0 +1,9 @@
+const CurrencyContract = artifacts.require('CurrencyContract');
+
+contract('CurrencyContract', accounts => {
+    describe('it should compile', () => {
+        it('should allow owner to call replenish function', async function() {
+            console.log('running');
+        });
+    });
+});
