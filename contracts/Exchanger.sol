047a1c40e795be00a8c3692aa451f25a5aeebd05
nvalidov <niyazvalidov@gmail.com>
2019-04-06 17:36:17 +0300

    check sign

--- contracts/Exchanger.sol
+++ contracts/Exchanger.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.5.0;
 
+import "CurrencyContract.sol";
+
 contract Exchanger {
     address public backend;
 
@@ -15,33 +17,81 @@ contract Exchanger {
 
     struct Order {
         uint256 valueLeft;
-        bool isInited;
+        bool isInitialized;
         bool isActive;
     }
 
     mapping (address => mapping (uint32 => Order)) public orders;
 
     function exchange(
-        address _seller,
+        address _sellerAddress,
         uint32 _sellerNonce,
         uint256 _sellerValue,
         uint256 _sellerRate,
-        bytes32 _sellerSign,
-        address _buyer,
+        bool _sellerDirection,
+        bytes memory _sellerSign,
+        address _buyerAddress,
         uint32 _buyerNonce,
         uint256 _buyerValue,
         uint256 _buyerRate,
-        bytes32 _buyerSign,
+        bool _buyerDirection,
+        bytes memory _buyerSign,
         uint256 _tradeValue,
         uint256 _tradeRate
     )
         onlyBackend
         external
     {
-        // exchange method
+
+    }
+
+    function _splitSignature(bytes memory sig) internal pure returns (uint8, bytes32, bytes32) {
+        require(sig.length == 65);
+
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
+
+        assembly {
+            r := mload(add(sig, 32))
+            s := mload(add(sig, 64))
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        return (v, r, s);
+    }
+
+    function _recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = _splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
+    function _prefixed(bytes32 hash) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash));
+    }
+
+    function _checkSignature(
+        address _signer,
+        uint32 _nonce,
+        uint256 _value,
+        uint256 _rate,
+        bytes memory _sign
+    )
+        internal
+        returns (bool)
+    {
+        bytes32 message = _prefixed(keccak256(abi.encodePacked(_nonce, _value, _rate)));
+
+        address signedBy = _recoverSigner(message, _sign);
+        return signedBy == _signer;
     }
 
     function cancelOrder(uint32 _nonce) external {
-        orders[msg.sender][_nonce] = Order({valueLeft: 0, isInited: true, isActive: false});
+        orders[msg.sender][_nonce] = Order({valueLeft: 0, isInitialized: true, isActive: false});
     }
 }
